7426c26d386d07aceb3e501a215b0333f9aaf583
Natalie Wolfe <natalie@lifewanted.com>
2015-03-07 17:12:20 -0800

    Promises can now resolve with values.

--- source/lw/event/Promise.hpp
+++ source/lw/event/Promise.hpp
@@ -7,16 +7,24 @@
 namespace lw {
 namespace event {
 
+template< typename T >
 class Future;
 
 /// @brief `Promise`s and `Future`s allow for chaining callbacks without nesting.
 ///
 /// A `Promise` is the active side of the pair. Asynchronous functions create a
 /// promise and later fulfill it by either resolving or rejecting the promise.
+template< typename T >
 class Promise {
 public:
     /// @brief Default construction.
-    Promise( void );
+    Promise( void ):
+        m_state( new _SharedState() )
+    {
+        m_state->resolved   = false;
+        m_state->rejected   = false;
+        m_state->resolve    = nullptr;
+    }
 
     // ---------------------------------------------------------------------- //
 
@@ -26,22 +34,38 @@ public:
     // ---------------------------------------------------------------------- //
 
     /// @brief Moves the promise from `other` to `this`.
-    Promise( Promise&& other );
+    Promise( Promise&& other ):
+        m_state( std::move( other.m_state ) )
+    {
+        other.m_state = nullptr;
+    }
 
     // ---------------------------------------------------------------------- //
 
     /// @brief Returns a future associated with this promise.
-    Future future( void );
+    Future< T > future( void ){
+        return Future< T >( m_state );
+    }
 
     // ---------------------------------------------------------------------- //
 
     /// @brief Resolves the promise as a success.
-    void resolve( void );
+    void resolve( T&& value ){
+        m_state->resolved = true;
+        if( m_state->resolve ){
+            m_state->resolve( std::move( value ) );
+        }
+    }
 
     // ---------------------------------------------------------------------- //
 
     /// @brief Rejects the promise as a failure.
-    void reject( void );
+    void reject( void ){
+        m_state->rejected = true;
+        if( m_state->reject ){
+            m_state->reject();
+        }
+    }
 
     // ---------------------------------------------------------------------- //
 
@@ -51,9 +75,14 @@ public:
     // ---------------------------------------------------------------------- //
 
     /// @brief Moves the promise from `other` into `this`.
-    Promise& operator=( Promise&& other );
+    Promise& operator=( Promise&& other ){
+        m_state = std::move( other.m_state );
+        other.m_state = nullptr;
+        return *this;
+    }
 
 private:
+    template< typename Type >
     friend class ::lw::event::Future;
 
     // ---------------------------------------------------------------------- //
@@ -62,7 +91,8 @@ private:
     struct _SharedState {
         std::atomic_bool resolved;
         std::atomic_bool rejected;
-        std::function< void( void ) > handler;
+        std::function< void( T&& ) > resolve;
+        std::function< void( void ) > reject;
     };
     typedef std::shared_ptr< _SharedState > _SharedStatePtr;
 
@@ -78,24 +108,27 @@ private:
 ///
 /// `Future`s are the requester's handle on an asynchronous event. They allow
 /// callbacks to be registered for after it has been started.
+template< typename T >
 class Future {
 public:
     /// @brief Chaining for generic functors.
     ///
-    /// @tparam Func A functor type that can take a `Promise&&` as its parameter.
+    /// @tparam Result  The type given functor promises.
+    /// @tparam Func    A functor type that can take a `Promise&&` as its parameter.
     ///
     /// @param func The functor to call when this one is resolved.
     ///
     /// @return A new future, for when the provided `func` completes its action.
     template<
+        typename Result,
         typename Func,
-        typename = typename std::result_of< Func( Promise&& ) >::type
+        typename = typename std::result_of< Func( T&&, Promise< Result >&& ) >::type
     >
-    Future then( Func&& func ){
-        auto next = std::make_shared< Promise >();
+    Future< Result > then( Func&& func ){
+        auto next = std::make_shared< Promise< T > >();
         auto prev = m_state;
-        m_state->handler = [ func, prev, next ]() mutable {
-            func( std::move( *next ) );
+        m_state->resolve = [ func, prev, next ]( T&& value ) mutable {
+            func( std::move( value ), std::move( *next ) );
             prev.reset();
         };
         return next->future();
@@ -111,15 +144,16 @@ public:
     ///
     /// @return A `Future` which will be resolved by `func`.
     template<
+        typename Result,
         typename Func,
-        typename FuncResult = typename std::result_of< Func() >::type,
+        typename FuncResult = typename std::result_of< Func( T&& ) >::type,
         typename std::enable_if<
-            std::is_base_of< Future, FuncResult >::value
+            std::is_base_of< Future< Result >, FuncResult >::value
         >::type* = nullptr
     >
-    Future then( Func&& func ){
-        return then([ func ]( Promise&& promise ){
-            func().then( std::move( promise ) );
+    Future< Result > then( Func&& func ){
+        return then< Result >([ func ]( T&& value, Promise< Result >&& promise ){
+            func( std::move( value ) ).then( std::move( promise ) );
         });
     }
 
@@ -128,11 +162,22 @@ public:
     /// @brief Connects this promise to the one provided.
     ///
     /// @param promise The promise to resolve/reject with this one.
-    void then( Promise&& promise );
+    void then( Promise< T >&& promise ){
+        auto next = std::make_shared< Promise< T > >( std::move( promise ) );
+        auto prev = m_state;
+        m_state->resolve = [ prev, next ]( T&& value ) mutable {
+            if( next ){
+                next->resolve( std::move( value ) );
+            }
+            prev.reset();
+        };
+    }
+
 
     // ---------------------------------------------------------------------- //
 
 private:
+    template< typename Type >
     friend class ::lw::event::Promise;
 
     // ---------------------------------------------------------------------- //
@@ -140,7 +185,7 @@ private:
     /// @brief Only `Promise`s can construct us.
     ///
     /// @param state The shared state to associate with.
-    Future( const Promise::_SharedStatePtr& state ):
+    Future( const typename Promise< T >::_SharedStatePtr& state ):
         m_state( state )
     {}
 
@@ -161,7 +206,7 @@ private:
     // ---------------------------------------------------------------------- //
 
     /// @brief Our internal shared state.
-    Promise::_SharedStatePtr m_state;
+    typename Promise< T >::_SharedStatePtr m_state;
 };
 
 }
