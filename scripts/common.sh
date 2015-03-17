06ea391cea10e8ce7b98a2719aa3ccc49d3569fc
Natalie Wolfe <natalie@lifewanted.com>
2015-03-17 13:02:34 -0700

    Fixed installing gcc

--- scripts/common.sh
+++ scripts/common.sh
@@ -12,12 +12,4 @@ function is_executable(){
     [ -f "$1" ] && [ -x "$1" ]
 }
 
-if exe_exists apt-get; then
-    export LW_APT=1
-    export LW_INSTALL=apt-get install
-elif exe_exists yum; then
-    export LW_YUM=1
-    export LW_INSTALL=yum install
-fi
-
 export -f exe_exists
--- scripts/install-gcc.sh
+++ scripts/install-gcc.sh
@@ -20,34 +20,40 @@ function get_gcc_version(){
     echo $gcc_version | grep -o '[[:digit:]]\.[[:digit:]]'
 }
 
-if [ LW_APT ]; then
-    function install_gcc(){
-        if [ ! has_gcc ]; then
-            sudo LW_INSTALL gcc g++
-        fi
+function install_gcc_apt(){
+    if ! has_gcc; then
+        sudo apt-get gcc g++
+    fi
 
-        if [ ! has_right_gcc_version ]; then
-            gcc_version=$(get_gcc_version)
+    if ! has_right_gcc_version; then
+        gcc_version=$(get_gcc_version)
 
-            sudo apt-add-repository --yes ppa:ubuntu-toolchain-r/test
-            sudo apt-get update
-            sudo apt-get install gcc-4.9 g++-4.9
-            sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-$gcc_version 40 --slave /usr/bin/g++ g++ /usr/bin/g++-$gcc_version
-            sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
-            sudo update-alternatives --auto gcc
-        fi
-    }
-else
-    function install_gcc(){
-        if [ ! has_gcc ]; then
-            sudo LW_INSTALL gcc gcc-c++
-        fi
+        sudo apt-add-repository --yes ppa:ubuntu-toolchain-r/test
+        sudo apt-get update
+        sudo apt-get install gcc-4.9 g++-4.9
+        sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-$gcc_version 40 --slave /usr/bin/g++ g++ /usr/bin/g++-$gcc_version
+        sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
+        sudo update-alternatives --auto gcc
+    fi
+}
 
-        if [ ! has_right_gcc_version ]; then
-            echo " !! Cannot install gcc 4.9 on this platform (do not know how)" >&2
-            return 1
-        fi
-    }
-fi
+function install_gcc_yum(){
+    if [ ! has_gcc ]; then
+        sudo yum install gcc gcc-c++
+    fi
+
+    if [ ! has_right_gcc_version ]; then
+        echo " !! Cannot install gcc 4.9 on this platform (do not know how)" >&2
+        return 1
+    fi
+}
+
+function install_gcc(){
+    if exe_exists; then
+        install_gcc_apt
+    else
+        install_gcc_yum
+    fi
+}
 
 export -f install_gcc
