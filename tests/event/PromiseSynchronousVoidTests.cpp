0f0fcb503e574dd8631910268ad25edf87fb7ac4
Natalie Wolfe <natalie@lifewanted.com>
2015-03-18 12:50:58 -0700

    Expanded PomiseSynchronousVoidTests

--- tests/event/PromiseSynchronousVoidTests.cpp
+++ tests/event/PromiseSynchronousVoidTests.cpp
@@ -12,7 +12,127 @@ struct PromiseSynchronousVoidTests : public testing::Test {
 
 // -------------------------------------------------------------------------- //
 
-TEST_F( PromiseSynchronousVoidTests, IntVoidTest ){
+TEST_F( PromiseSynchronousVoidTests, Void_PromiseInt ){
+    // State observation variables.
+    bool chained    = false;
+    bool resolved   = false;
+    bool resolved2  = false;
+
+    const int value = 42;
+
+    // Create the promise and assign a resolve handler.
+    event::Promise<> prom;
+    prom.future().then< int >([&]( event::Promise< int >&& prom2 ){
+        EXPECT_TRUE( chained );
+        EXPECT_FALSE( resolved );
+        EXPECT_FALSE( resolved2 );
+
+        resolved = true;
+        prom2.resolve( value );
+    }).then([&]( int result ){
+        EXPECT_TRUE( chained );
+        EXPECT_TRUE( resolved );
+        EXPECT_FALSE( resolved2 );
+        EXPECT_EQ( value, result );
+
+        resolved2 = true;
+    });
+
+    // The handler is chained, check that it hasn't been run.
+    chained = true;
+    EXPECT_FALSE( resolved );
+    EXPECT_FALSE( resolved2 );
+
+    // Resolve the promise and check that the resolved handler is true
+    prom.resolve();
+    EXPECT_TRUE( resolved );
+    EXPECT_TRUE( resolved2 );
+}
+
+// -------------------------------------------------------------------------- //
+
+TEST_F( PromiseSynchronousVoidTests, Void_PromiseVoid ){
+    // State observation variables.
+    bool chained    = false;
+    bool resolved   = false;
+    bool resolved2  = false;
+
+    // Create the promise and assign a resolve handler.
+    event::Promise<> prom;
+    prom.future().then([&]( event::Promise<>&& prom2 ){
+        EXPECT_TRUE( chained );
+        EXPECT_FALSE( resolved );
+        EXPECT_FALSE( resolved2 );
+
+        resolved = true;
+        prom2.resolve();
+    }).then([&](){
+        EXPECT_TRUE( chained );
+        EXPECT_TRUE( resolved );
+        EXPECT_FALSE( resolved2 );
+
+        resolved2 = true;
+    });
+
+    // The handler is chained, check that it hasn't been run.
+    chained = true;
+    EXPECT_FALSE( resolved );
+    EXPECT_FALSE( resolved2 );
+
+    // Resolve the promise and check that the resolved handler is true
+    prom.resolve();
+    EXPECT_TRUE( resolved );
+    EXPECT_TRUE( resolved2 );
+}
+
+// -------------------------------------------------------------------------- //
+
+TEST_F( PromiseSynchronousVoidTests, FutureInt_Void ){
+    // State observation variables.
+    bool chained    = false;
+    bool resolved   = false;
+    bool resolved2  = false;
+
+    const int value = 42;
+
+    // Create the promise and assign a resolve handler.
+    event::Promise<> prom;
+    event::Promise< int > prom2;
+    prom.future().then([&](){
+        EXPECT_TRUE( chained );
+        EXPECT_FALSE( resolved );
+        EXPECT_FALSE( resolved2 );
+
+        resolved = true;
+        return prom2.future();
+    }).then([&]( int result ){
+        EXPECT_TRUE( chained );
+        EXPECT_TRUE( resolved );
+        EXPECT_FALSE( resolved2 );
+        EXPECT_EQ( value, result );
+
+        resolved2 = true;
+    });
+
+    // The handler is chained, check that it hasn't been run.
+    chained = true;
+    EXPECT_FALSE( resolved );
+    EXPECT_FALSE( resolved2 );
+
+    // Resolve the promise and check that the resolved handler is true
+    prom.resolve();
+    EXPECT_TRUE( resolved );
+    EXPECT_FALSE( resolved2 );
+
+    // Now resolve the 2nd promise.
+    prom2.resolve( value );
+    EXPECT_TRUE( resolved );
+    EXPECT_TRUE( resolved2 );
+}
+
+// -------------------------------------------------------------------------- //
+
+TEST_F( PromiseSynchronousVoidTests, Int_Void ){
     // State observation variables.
     bool chained    = false;
     bool resolved   = false;
@@ -51,19 +171,56 @@ TEST_F( PromiseSynchronousVoidTests, IntVoidTest ){
 
 // -------------------------------------------------------------------------- //
 
-TEST_F( PromiseSynchronousVoidTests, VoidVoidTest ){
+TEST_F( PromiseSynchronousVoidTests, Void_Void ){
     // State observation variables.
     bool chained    = false;
     bool resolved   = false;
+    bool resolved2  = false;
 
     // Create the promise and assign a resolve handler.
     event::Promise<> prom;
     prom.future().then([&](){
         EXPECT_TRUE( chained );
         EXPECT_FALSE( resolved );
+        EXPECT_FALSE( resolved2 );
+
+        resolved = true;
+    }).then([&](){
+        EXPECT_TRUE( chained );
+        EXPECT_TRUE( resolved );
+        EXPECT_FALSE( resolved2 );
+
+        resolved2 = true;
+    });
+
+    // The handler is chained, check that it hasn't been run.
+    chained = true;
+    EXPECT_FALSE( resolved );
+    EXPECT_FALSE( resolved2 );
+
+    // Resolve the promise and check that the resolved handler is true
+    prom.resolve();
+    EXPECT_TRUE( resolved );
+    EXPECT_TRUE( resolved2 );
+}
+
+// -------------------------------------------------------------------------- //
+
+TEST_F( PromiseSynchronousVoidTests, ConnectPromise ){
+    // State observation variables.
+    bool chained    = false;
+    bool resolved   = false;
+
+    // Create the promise and assign a resolve handler.
+    event::Promise<> prom;
+    event::Promise<> prom2;
+    prom2.future().then([&](){
+        EXPECT_TRUE( chained );
+        EXPECT_FALSE( resolved );
 
         resolved = true;
     });
+    prom.future().then( std::move( prom2 ) );
 
     // The handler is chained, check that it hasn't been run.
     chained = true;
