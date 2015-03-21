f232c0f9722682de9777c6bad14c2ac94a9da3c1
Natalie Wolfe <natalie@lifewanted.com>
2015-03-21 10:39:29 -0700

    Added start of error module.

---
+++ source/lw/error.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "lw/error/Exception.hpp"
---
+++ source/lw/error/Exception.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdexcept>
+
+namespace lw {
+namespace error {
+
+class Exception : public std::runtime_error {
+public:
+    Exception( const std::int64_t code, const std::string& message ):
+        std::runtime_error( message ),
+        m_error_code( code )
+    {}
+
+    // ---------------------------------------------------------------------- //
+
+    std::int64_t error_code( void ) const {
+        return m_error_code;
+    }
+
+    // ---------------------------------------------------------------------- //
+
+private:
+    std::int64_t m_error_code;
+};
+
+}
+}
