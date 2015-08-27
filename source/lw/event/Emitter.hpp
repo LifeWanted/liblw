65307d287ef9910fd2152f73771f15c4c6310760
Natalie Wolfe <natalie@lifewanted.com>
2015-08-26 19:00:28 -0700

    Documentation

--- source/lw/event/Emitter.hpp
+++ source/lw/event/Emitter.hpp
@@ -13,6 +13,242 @@
 namespace lw {
 namespace event {
 
+/// @brief Implementation details for event emitters.
+namespace _details {
+    template<
+        std::size_t I,
+        typename EventId,
+        typename Event,
+        typename OtherEvents...,
+        typename = typename std::enable_if<
+            !std::is_same<EventId, typename Event::event_id_type>::value
+        >::type
+    >
+    struct EventIndexImpl : public EventIndexImpl<I + 1, EventId, OtherEvents...> {};
+
+    template<
+        std::size_t I,
+        typename EventId,
+        typename Event,
+        typename OtherEvents...,
+        typename = typename std::enable_if<
+            std::is_same<EventId, typename Event::event_id_type>::value
+        >::type
+    >
+    struct EventIndexImpl : public std::integral_constant<std::size_t, I> {};
+
+    template<typename EventId, typename... Events>
+    struct EventIndex : public EventIndexImpl<0, EventId, Events...> {};
+
+    template<typename EventId, typename... Events>
+    struct GetEvent {
+        typedef EventIndex<EventId, Events...> event_idx_type;
+        typedef std::tuple<Events...> event_tuple_type;
+        typedef typename std::tuple_element<event_idx_type::value, event_tuple_type>::type event_type;
+
+        static event_type& from(event_tuple_type& events){
+            return std::get<event_idx_type::value>(events);
+        }
+
+        static const event_type& from(const event_tuple_type& events){
+            return std::get<event_idx_type::value>(events);
+        }
+    };
+
+    template<typename Func, typename Args>
+    struct IsCallable : public std::false_type {};
+
+    template<typename Func, typename... Args, typename = typename std::result_of<Func(Args&&...)>::type>
+    struct IsCallback<Func, std::tuple<Args...>> : public std::true_type {};
+
+    template<typename Listener, typename Event>
+    struct EventListenerCallable : public IsCallable<Listener, typename Event::event_argument_types> {};
+}
+
+// ---------------------------------------------------------------------------------------------- //
+
+/// @brief Maintains all information about a single event, including bound listeners.
+///
+/// @tparam EventArgs The arguments listeners expected with this event.
+template<typename... EventArgs>
+class Event {
+public:
+    typedef std::tuple<EventArgs...> event_argument_types;      ///< The event arguments.
+    typedef std::function<void(EventArgs&&...)> listener_type;  ///< The type for storing listeners.
+
+    /// @brief Adds a new event listener to the back of the list.
+    ///
+    /// @tparam Listener The event listener type which must conform to `listener_type`.
+    ///
+    /// @param listener The event listener being added.
+    template<typename Listener>
+    void emplace_back(Listener&& listener){
+        m_listeners.emplace_back(std::forward<Listener>(listener));
+    }
+
+    /// @copydoc emplace_back
+    template<typename Listener>
+    void push_back(Listener&& listener){
+        m_listeners.push_back(std::forward<Listener>(listener));
+    }
+
+    /// @brief Calls all the listeners with the provided arguments.
+    ///
+    /// @tparam CalledArgs
+    ///     The types of the provided arguements. Must be compatible with `event_argument_types`.
+    ///
+    /// @param args The argument values being passed to each listener.
+    template<typename... CalledArgs>
+    void operator()(CalledArgs&&... args){
+        for (auto&& listener : m_listeners) {
+            listener(args...);
+        }
+    }
+
+    /// @brief Removes any listeners for which the predicate returns true.
+    ///
+    /// @tparam Pred A unary predicate type which takes a `listener_type` instance.
+    ///
+    /// @param pred
+    ///     The predicate receiving each listener in turn. If it returns `true` the listener will be
+    ///     removed from the event.
+    template<typename Pred>
+    void remove_if(Pred&& pred){
+        m_listeners.remove_if(std::forward<Pred>(pred));
+    }
+
+    /// @brief Removes any listeners equal to the one provided.
+    ///
+    /// @tparam Listener The listener type to remove.
+    ///
+    /// @param listener The value to check for.
+    template<typename Listener>
+    void remove(Listener&& listener){
+        m_listeners.remove(std::forward<Listener>(listener));
+    }
+
+    /// @brief Removes all listeners from the event.
+    void clear(void){
+        m_listeners.clear();
+    }
+
+    /// @brief Returns the number of listeners bound to this event.
+    std::size_t count(void) const {
+        return m_listeners.count();
+    }
+
+    /// @brief Returns true if no listeners are bound to this event.
+    bool empty(void) const {
+        return m_listeners.empty();
+    }
+
+private:
+    std::list<listener_type> m_listeners; ///< The list of event listeners.
+};
+
+/// @brief Adds a type-safe method for identifying events.
+///
+/// These are the event types used by the `Emitter` class.
+///
+/// @tparam EventId The type used to identify this event.
+template<typename EventId, typename... EventArgs>
+class IdEvent : public Event<EventArgs...> {
+public:
+    typedef EventId event_id_type; ///< The event Id type.
+};
+
+/// @brief A class which can emit several different event types.
+///
+/// @tparam Events All the events this emitter will support. Must be derived from `IdEvent`.
+template<typename... Events>
+class Emitter {
+public:
+    typedef std::tuple<Events...> event_types; ///< The events supported by the emitter.
+
+    /// @brief
+    template<
+        typename EventId,
+        typename Listener,
+        typename = typename std::enable_if<
+            _details::EventListenerCallable<
+                Listener, typename _details::GetEvent<EventId, Events...>::type
+            >::value
+        >::type
+    >
+    void on(const EventId&, Listener&& listener){
+        _details::GetEvent<EventId, Events...>::from(m_events)
+            .emplace_back(std::forward<Listener>(listener));
+    }
+
+    template<typename EventId, typename Listener>
+    void emplace(const EventId& e, Listener&& listener){
+        on(e, std::forward<Listener>(listener));
+    }
+
+    template<
+        typename EventId,
+        typename Listener,
+        typename = typename std::enable_if<
+            _details::EventListenerCallable<
+                Listener, typename _details::GetEvent<EventId, Events...>::type
+            >::value
+        >::type
+    >
+    void insert(const EventId&, Listener&& listener){
+        _details::GetEvent<EventId, Events...>::from(m_events)
+            .push_back(std::forward<Listener>(listener));
+    }
+
+    template<typename EventId, typename Pred>
+    void remove_if(const EventId&, Pred&& pred){
+        _details::GetEvent<EventId, Events...>::from(m_events).remove_if(std::forward<Pred>(pred));
+    }
+
+    template<typename EventId, typename Listener>
+    void remove(const EventId&, Listener&& listener){
+        _details::GetEvent<EventId, Events...>::from(m_events)
+            .remove(std::forward<Listener>(listener));
+    }
+
+    template<typename EventId>
+    void remove_all(const EventId&){
+        _details::GetEvent<EventId, Events...>::from(m_events).remove_all();
+    }
+
+    void remove_all(const EventId&){
+        ....???
+    }
+
+    template<typename EventId>
+    void clear(const EventId&){
+        _details::GetEvent<EventId, Events...>::from(m_events).clear();
+    }
+
+    void clear(void){
+        ....???
+    }
+
+    bool empty(void) const {
+        ....???
+    }
+
+    template<typename EventId>
+    std::size_t count(const EventId&) const {
+        return _details::GetEvent<EventId, Events...>::from(m_events).count();
+    }
+
+protected:
+    template<typename EventId, typename... Args>
+    void emit(const EventId&, Args&&...){
+        _details::GetEvent<EventId, Events...>::from(m_events)(std::forward<Args>(args)...);
+    }
+
+private:
+    event_types m_events;
+};
+
+// ---------------------------------------------------------------------------------------------- //
+
 // Declares event types.
 #define LW_DECLARE_EVENT(_event_name) struct LW_CONCAT(_event_name, _t) {};
 #define LW_DECLARE_EVENTS(...) LW_FOR_EACH(LW_DECLARE_EVENT, __VA_ARGS__)
@@ -23,107 +259,15 @@ namespace event {
 #define LW_DEFINE_EVENTS(...) LW_FOR_EACH(LW_DEFINE_EVENT, __VA_ARGS__)
 
 // Creates an event emitter class using the given events.
-#define _LW_EVENT_TYPE(_event_name, ...) ::lw::event::Event<LW_CONCAT(_event_name, _t), __VA_ARGS__>,
+#define _LW_EVENT_TYPE(_event_name, ...) ::lw::event::IdEvent<LW_CONCAT(_event_name, _t), __VA_ARGS__>,
 #define _LW_IMPORT_EVENT(_event_name, ...) static constexpr LW_CONCAT(_event_name, _t) _event_name{};
-#define _LW_EVENT_STRING_CALL(_event_name, ...)             \
-    if (event == LW_STRINGIFY(_event_name)) {               \
-        on(_event_name, std::forward<Listener>(listener));  \
-        return;                                             \
-    }
 #define LW_DEFINE_EMITTER(_emitter_name, ...)                                           \
     class _emitter_name :                                                               \
         public ::lw::event::Emitter<LW_FOR_EACH(_LW_EVENT_TYPE, __VA_ARGS__) nullptr_t> \
     {                                                                                   \
     public:                                                                             \
         struct event { LW_FOR_EACH(_LW_IMPORT_EVENT, __VA_ARGS__) };                    \
-                                                                                        \
-        template<typename Listener>                                                     \
-        void on(const std::string& event, Listener&& listener){                         \
-            LW_FOR_EACH(_LW_EVENT_STRING_CALL, __VA_ARGS__)                             \
-        }                                                                               \
     };
-
-// ---------------------------------------------------------------------------------------------- //
-
-template<typename EventId, typename... EventArgs>
-class Event {
-public:
-    typedef EventId event_id_type;
-    typedef std::tuple<EventArgs...> event_argument_types;
-    typedef std::function<void(EventArgs&&...)> listener_type;
-
-private:
-    std::list<listener_type> m_listeners;
-};
-
-template<
-    std::size_t I,
-    typename EventId,
-    typename Event,
-    typename OtherEvents...,
-    typename = typename std::enable_if<
-        !std::is_same<EventId, typename Event::event_id_type>::value
-    >::type
->
-struct EventIndexImpl : public EventIndexImpl<I + 1, EventId, OtherEvents...> {};
-
-template<
-    std::size_t I,
-    typename EventId,
-    typename Event,
-    typename OtherEvents...,
-    typename = typename std::enable_if<
-        std::is_same<EventId, typename Event::event_id_type>::value
-    >::type
->
-struct EventIndexImpl : public std::integral_constant<std::size_t, I> {};
-
-template<typename EventId, typename... Events>
-struct EventIndex : public EventIndexImpl<0, EventId, Events...> {};
-
-template<typename EventId, typename... Events>
-struct GetEvent {
-    typedef EventIndex<EventId, Events...> event_idx_type;
-    typedef std::tuple<Events...> event_tuple_type;
-    typedef typename std::tuple_element<event_idx_type::value, event_tuple_type>::type event_type;
-
-    static event_type& from(event_tuple_type& events){
-        return std::get<event_idx_type::value>(events);
-    }
-
-    static const event_type& from(const event_tuple_type& events){
-        return std::get<event_idx_type::value>(events);
-    }
-};
-
-template<typename Func, typename Args>
-struct IsCallable : public std::false_type {};
-
-template<typename Func, typename... Args, typename = typename std::result_of<Func(Args&&...)>::type>
-struct IsCallback<Func, std::tuple<Args...>> : public std::true_type {};
-
-template<typename Listener, typename Event>
-struct EventListenerCallable : public IsCallable<Listener, typename Event::event_argument_types> {};
-
-template<typename... Events>
-class Emitter {
-public:
-    typedef std::tuple<Events...> event_types;
-
-    template<
-        typename EventId,
-        typename Listener,
-        typename = typename std::enable_if<
-            EventListenerCallable<Listener, typename GetEvent<EventId, Events...>::type>::value
-        >::type
-    >
-    void on(const EventId&, Listener&& listener){
-        GetEvent<EventId, Events...>::from(m_events).add_listener(std::forward<Listener>(listener));
-    }
-
-private:
-    event_types m_events;
-};
 
 }
 }
