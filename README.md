a0908e5039a6b8267880aee77107424ba411870d
Natalie Wolfe <natalie@lifewanted.com>
2020-01-25 20:02:47 -0800

    Adding first module.

---
+++ README.md
@@ -0,0 +1,8 @@
+# liblw
+
+A coroutine-ready asychronous web framework in C++.
+
+## Setup
+
+1. Install Bazel: https://docs.bazel.build/versions/master/install.html
+2. Ensure you have a compiler supporting C++20 (e.g. gcc-9 or later).
---
+++ lw/io/stream/buffer.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <streambuf>
+#include <string>
+
+namespace lw::io::stream {
+
+class StringBuffer : public std::streambuf {
+public:
+  StringBuffer();
+  StringBuffer(StringBuffer&&);
+  StringBuffer(const StringBuffer&) = delete;
+  StringBuffer& operator=(StringBuffer&&);
+  StringBuffer& operator=(const StringBuffer&) = delete;
+
+  char_type* data() { return _buffer.data(); }
+  const char_type* data() const { return _buffer.data(); }
+  const char_type* c_str() const { return _buffer.c_str(); }
+  const std::size_t size() const { return _buffer.size(); }
+  const std::size_t capacity() const { return _buffer.capacity(); }
+  const std::string& string() const { return _buffer; }
+
+  void reserve(std::size_t new_capacity);
+
+protected:
+  std::streamsize _get_size() const { return gptr() - _buffer.data(); }
+  std::streamsize showmanyc() override { return _get_size(); }
+  std::streamsize xsgetn(char_type* out, std::streamsize count) override;
+  std::streamsize xsputn(const char_type* in, std::streamsize count) override;
+
+private:
+  std::string _buffer;
+};
+
+}
