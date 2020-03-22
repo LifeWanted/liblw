0e68688010d18f4835a9062953863a74c2a22b50
Natalie Wolfe <natalie@lifewanted.com>
2020-03-22 11:54:32 -0700

    Updated Buffer class and readded.

---
+++ lw/memory/buffer.h
@@ -0,0 +1,191 @@
+#pragma once
+
+#include <algorithm>
+#include <cstdint>
+
+namespace lw {
+
+/**
+ * A buffer that can store dynamically or statically allocated memory.
+ *
+ * The buffers can be built around existing memory blocks (optionally taking
+ * ownership of them) or can allocate their own blocks.
+ *
+ * There are no protections in place for reading or writing outside the bounds
+ * of the buffer.
+ */
+class Buffer {
+public:
+  Buffer() = default;
+  Buffer(const Buffer&) = delete;
+  Buffer& operator=(const Buffer&) = delete;
+
+  /**
+   * Create a buffer wrapped around externally allocated memory, optionally
+   * taking ownership of the memory.
+   *
+   * @param buffer
+   *  A pointer to the beginning of the buffer.
+   * @param capacity
+   *  The size of the buffer in bytes.
+   * @param own_data
+   *  Flag indicating if this `Buffer` should take ownership of the memory.
+   */
+  Buffer(std::uint8_t* buffer, std::size_t capacity, bool own_data = false):
+    _capacity{capacity},
+    _data{buffer},
+    _own_data{own_data}
+  {}
+
+  /**
+   * Move ownership from other to this.
+   *
+   * The ownership of the data is transfered to this buffer, but the `other`
+   * buffer remains pointing to the same block with the same capacity so it
+   * can continue being used. You are not guaranteed that the data in `other`
+   * will remain valid as this buffer may free the memory if it is destroyed.
+   */
+  Buffer(Buffer&& other):
+    _capacity{other._capacity},
+    _data{other._data},
+    _own_data{other._own_data}
+  {
+    other._own_data = false;
+  }
+
+  /**
+   * Create a buffer which allocates its own memory.
+   *
+   * @param size The number of bytes to allocate.
+   */
+  explicit Buffer(const std::size_t& size):
+    _capacity{size},
+    _data{new std::uint8_t[size]},
+    _own_data{true}
+  {}
+
+  /**
+   * Generic data-copying constructor.
+   *
+   * The input iterator must support differences
+   * (`operator-(const InputIterator&, const InputIterator&)`) as a way of
+   * determining the distance between two iterators. On top of that it must
+   * support dereferencing (`operator*()`) and prefix increment
+   * (`operator++()`).
+   *
+   * @tparam InputIterator
+   *  A generic forward-iterator whose dereferenced value must be convertible
+   *  to `byte`.
+   *
+   * @param begin
+   *  The iterator to begin the copy from.
+   * @param end
+   *  An iterator just past the end of the data.
+   */
+  template <typename InputIterator>
+  Buffer(InputIterator begin, const InputIterator& end):
+    Buffer{(std::size_t)(end - begin)}
+  {
+    std::copy(begin, end, _data);
+  }
+
+  ~Buffer() {
+    if (_own_data && _data) {
+      delete[] _data;
+    }
+  }
+
+  std::size_t capacity() const { return _capacity; }
+  std::size_t size() const { return capacity(); }
+
+  std::uint8_t* data() { return _data; }
+  const std::uint8_t* data() const { return _data; }
+
+  std::uint8_t& operator[](std::size_t i) { return _data[i]; }
+  const std::uint8_t& operator[](std::size_t i) const { return _data[i]; }
+
+  std::uint8_t* begin() { return _data; }
+  const std::uint8_t* begin() const { return _data; }
+
+  std::uint8_t* end() { return _data + _capacity; }
+  const std::uint8_t* end() const { return _data + _capacity; }
+
+  std::uint8_t& front();
+  const std::uint8_t& front() const;
+  std::uint8_t& back();
+  const std::uint8_t& back() const;
+
+  /**
+   * Sets all the bytes in the buffer to `val`.
+   *
+   * @param val
+   *  The byte value to set for all bytes in the buffer.
+   */
+  void set_memory(std::uint8_t val);
+
+  /**
+   * Copies at most `size()` bytes of data.
+   *
+   * @param begin
+   *  The iterator to start from.
+   * @param end
+   *  The iterator one past the end.
+   */
+  template <typename InputIterator>
+  void copy(InputIterator begin, const InputIterator& end) {
+    for (std::size_t i = 0; i < size() && begin != end; ++i, ++begin) {
+      _data[i] = *begin;
+    }
+  }
+
+  /**
+   * Copies `std::min(size(), count)` bytes of data.
+   *
+   * @param begin
+   *  The iterator to start from.
+   * @param count
+   *  The number of bytes to copy.
+   */
+  template <typename InputIterator>
+  void copy(InputIterator begin, const std::size_t count) {
+    std::copy_n(begin, std::min(count, size()), _data);
+  }
+
+  /**
+   * Moves control of a buffer over to this one.
+   *
+   * The ownership of the data is transfered to this buffer, but the `other`
+   * buffer remains pointing to the same block with the same capacity so it
+   * can continue being used. You are not guaranteed that the data in `other`
+   * will remain valid as this buffer may free the memory if it is destroyed.
+   */
+  Buffer& operator=(Buffer&& other);
+
+  /**
+   * Two buffers are equal if they are the same size and contain exactly the
+   * same data, byte for byte.
+   */
+  bool operator==(const Buffer& other) const;
+  bool operator!=(const Buffer& other) const {
+      return !(*this == other);
+  }
+
+  /**
+   * Combines two memory buffers using XOR.
+   *
+   * Only `std::min( this->size(), other.size() )` bytes are combined.
+   */
+  Buffer& operator^=(const Buffer& other);
+
+private:
+  /**
+   * Performs XOR between two buffers into a third.
+   */
+  static void _xor(const Buffer& lhs, const Buffer& rhs, Buffer* out);
+
+  std::size_t _capacity = 0;
+  std::uint8_t* _data = nullptr;
+  bool _own_data = false;
+};
+
+}
---
+++ lw/memory/buffer_test.cpp
@@ -0,0 +1,215 @@
+#include "lw/memory/buffer.h"
+
+#include <algorithm>
+#include <cstring>
+#include <gtest/gtest.h>
+#include <string>
+#include <tuple>
+
+namespace lw {
+namespace {
+
+TEST(Buffer, DefaultConstructor) {
+  Buffer buffer;
+  EXPECT_EQ(0, buffer.capacity());
+  EXPECT_EQ(0, buffer.size());
+  EXPECT_EQ(nullptr, buffer.data());
+}
+
+TEST(Buffer, WrapConstructor) {
+  char data[] = "Hello World!";
+  const std::size_t size = sizeof(data);
+  Buffer buffer(reinterpret_cast<std::uint8_t*>(data), size);
+
+  // Should adopt the provided character array.
+  EXPECT_EQ(size, buffer.size());
+  EXPECT_EQ(reinterpret_cast<std::uint8_t*>(data), buffer.data());
+
+  // Should not be a copy of the data.
+  buffer[0] = 'B';
+  EXPECT_EQ('B', data[0]);
+}
+
+TEST(Buffer, MoveConstructor) {
+  char data[] = "Hello World!";
+  const std::size_t size = sizeof(data);
+  Buffer* buffer = new Buffer(reinterpret_cast<std::uint8_t*>(data), size);
+  Buffer* buffer2 = new Buffer(std::move(*buffer));
+
+  EXPECT_EQ(size, buffer2->size());
+  EXPECT_EQ(reinterpret_cast<std::uint8_t*>(data), buffer2->data());
+
+  EXPECT_NO_THROW({ delete buffer; });
+
+  EXPECT_EQ(size, buffer2->size());
+  EXPECT_EQ(reinterpret_cast<std::uint8_t*>(data), buffer2->data());
+
+  EXPECT_NO_THROW({ delete buffer2; });
+}
+
+TEST(Buffer, MoveOwnedConstructor) {
+  const char* str = "Hello, World!";
+  const std::size_t size = std::strlen(str);
+  char* data = new char[50];
+  std::copy(str, str + size, data );
+  Buffer* buffer = new Buffer(reinterpret_cast<std::uint8_t*>(data), size, true);
+  Buffer* buffer2 = new Buffer(std::move(*buffer));
+
+  EXPECT_EQ(size, buffer2->size());
+  EXPECT_EQ(reinterpret_cast<std::uint8_t*>(data), buffer2->data());
+
+  EXPECT_NO_THROW({ delete buffer; });
+
+  EXPECT_EQ(size, buffer2->size());
+  EXPECT_EQ(reinterpret_cast<std::uint8_t*>(data), buffer2->data());
+
+  EXPECT_NO_THROW({ delete buffer2; });
+}
+
+TEST(Buffer, SizedAllocationConstructor) {
+  Buffer buffer(25);
+  EXPECT_EQ(25, buffer.size());
+}
+
+TEST(Buffer, IterableCopyConstructor) {
+  char data[] = "Hello World!";
+  Buffer buffer(std::begin(data), std::end(data));
+
+  // Should be the same size, but different data addresses.
+  EXPECT_EQ(sizeof(data), buffer.size());
+  EXPECT_NE((void*)data, (void*)buffer.data());
+
+  // Should have copied the contents into itself.
+  std::size_t i = 0;
+  for (const auto& c : buffer) {
+    EXPECT_EQ(data[i], c) << "At position " << i;
+    ++i;
+  }
+
+  // Should not reflect changes.
+  EXPECT_EQ('H', buffer[0]);
+  EXPECT_EQ('H', data[0]);
+  data[0] = 'B';
+  EXPECT_EQ('H', buffer[0]);
+  EXPECT_EQ('B', data[0]);
+  buffer[0] = 'D';
+  EXPECT_EQ('D', buffer[0]);
+  EXPECT_EQ('B', data[0]);
+}
+
+TEST(Buffer, Destructor) {
+  std::uint8_t* data = new std::uint8_t[25];
+  Buffer* buffer = new Buffer(data, 25);
+
+  // Should not delete the associated data by default.
+  EXPECT_NO_THROW({ delete buffer; });
+  EXPECT_NO_THROW({ delete data; });
+}
+
+TEST(Buffer, OwnedDataDestructor) {
+  std::uint8_t* data = new std::uint8_t[25];
+  Buffer* buffer = new Buffer(data, 25, true);
+
+  // Should delete the associated data when told to own it.
+  EXPECT_NO_THROW({ delete buffer; });
+  // EXPECT_DEATH({ delete data; }, "pointer being freed was not allocated");
+}
+
+TEST(Buffer, SetMemory) {
+  Buffer buffer(25);
+  buffer.set_memory((std::uint8_t)'a');
+  for (const auto& c : buffer) {
+    EXPECT_EQ((std::uint8_t)'a', c);
+  }
+
+  buffer.set_memory((std::uint8_t)'f');
+  for (const auto& c : buffer) {
+    EXPECT_EQ((std::uint8_t)'f', c);
+  }
+}
+
+TEST(Buffer, BeginEndCopy) {
+  const std::uint8_t* bufferValue =
+    reinterpret_cast<const std::uint8_t*>("Hello\0\0\0\0\0");
+  char data[] = "Hello World!";
+  Buffer buffer(10);
+  Buffer buffer2(bufferValue, bufferValue + 10);
+  buffer.set_memory(0);
+
+  buffer.copy(
+    reinterpret_cast<std::uint8_t*>(data),
+    reinterpret_cast<std::uint8_t*>(data) + 5
+  );
+  EXPECT_EQ(buffer2, buffer);
+}
+
+TEST(Buffer, SizedCopy) {
+  const std::uint8_t* bufferValue =
+    reinterpret_cast<const std::uint8_t*>("Hello\0\0\0\0\0");
+  char data[] = "Hello World!";
+  Buffer buffer(10);
+  Buffer buffer2(bufferValue, bufferValue + 10);
+  buffer.set_memory(0);
+
+  buffer.copy(reinterpret_cast<std::uint8_t*>(data), 5);
+  EXPECT_EQ(buffer2, buffer);
+}
+
+TEST(Buffer, MoveOperator) {
+  char data[] = "Hello World!";
+  const std::size_t size = sizeof(data);
+  Buffer* buffer = new Buffer(reinterpret_cast<std::uint8_t*>(data), size);
+  Buffer* buffer2 = new Buffer();
+  *buffer2 = std::move(*buffer);
+
+  EXPECT_EQ(size, buffer2->size());
+  EXPECT_EQ(reinterpret_cast<std::uint8_t*>(data), buffer2->data());
+
+  EXPECT_NO_THROW({ delete buffer; });
+
+  EXPECT_EQ(size, buffer2->size());
+  EXPECT_EQ(reinterpret_cast<std::uint8_t*>(data), buffer2->data());
+
+  EXPECT_NO_THROW({ delete buffer2; });
+}
+
+TEST(Buffer, MoveOwnedOperator) {
+  const char* str = "Hello, World!";
+  const std::size_t size = std::strlen(str);
+  char* data = new char[50];
+  std::copy(str, str + size, data );
+  Buffer* buffer = new Buffer(reinterpret_cast<std::uint8_t*>(data), size, true);
+  Buffer* buffer2 = new Buffer();
+  *buffer2 = std::move(*buffer);
+
+  EXPECT_EQ(size, buffer2->size());
+  EXPECT_EQ(reinterpret_cast<std::uint8_t*>(data), buffer2->data());
+
+  EXPECT_NO_THROW({ delete buffer; });
+
+  EXPECT_EQ(size, buffer2->size());
+  EXPECT_EQ(reinterpret_cast<std::uint8_t*>(data), buffer2->data());
+
+  EXPECT_NO_THROW({ delete buffer2; });
+}
+
+TEST(Buffer, EqualityOperator) {
+  Buffer b1(10);
+  Buffer b2(10);
+  Buffer b3(10);
+  Buffer b4(20);
+  Buffer b5(b1.data(), b1.size());
+
+  b1.set_memory('a');
+  b2.set_memory('a');
+  b3.set_memory('f');
+  b4.set_memory('a');
+
+  EXPECT_EQ(b1, b2);
+  EXPECT_NE(b1, b3);
+  EXPECT_NE(b1, b4);
+  EXPECT_EQ(b1, b5);
+}
+
+}
+}
