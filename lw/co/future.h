d4e604abd914e124229e3580e2c8de51fc42c069
Natalie Wolfe <natalie@lifewanted.com>
2020-05-21 16:41:50 -0700

    Do not discard futures.

--- lw/co/future.h
+++ lw/co/future.h
@@ -9,13 +9,13 @@ namespace lw::co {
  * Creates a future that is already resolved with the given value.
  */
 template <typename T>
-std::future<T> make_future(T&& value) {
+[[nodiscard]] std::future<T> make_future(T&& value) {
   std::promise<T> promise;
   promise.set_value(std::forward<T>(value));
   return promise.get_future();
 }
 
-inline std::future<void> make_future() {
+[[nodiscard]] inline std::future<void> make_future() {
   std::promise<void> promise;
   promise.set_value();
   return promise.get_future();
@@ -25,7 +25,7 @@ inline std::future<void> make_future() {
  * Create a future that is rejected with the given exception.
  */
 template <typename T>
-std::future<T> make_future(std::exception_ptr err) {
+[[nodiscard]] std::future<T> make_future(std::exception_ptr err) {
   std::promise<T> promise;
   promise.set_exception(err);
   return promise.get_future();
