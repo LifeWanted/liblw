e8fa8db234e0963d48e987773a705db747adbba9
Natalie Wolfe <natalie@lifewanted.com>
2020-06-06 20:27:54 -0700

    Added internal error check for null

--- lw/err/macros.h
+++ lw/err/macros.h
@@ -3,5 +3,8 @@
 #include "lw/err/canonical.h"
 
 #define LW_CHECK_NULL(arg)  \
-  if ((arg) == nullptr)     \
+  while ((arg) == nullptr)  \
     throw lw::InvalidArgument() << #arg << " must not be null. "
+
+#define LW_CHECK_NULL_INTERNAL(arg) \
+  while ((arg) == nullptr) throw lw::Internal()
--- lw/err/macros_test.cpp
+++ lw/err/macros_test.cpp
@@ -16,9 +16,24 @@ TEST(ErrMacros, NullCheckThrowsOnNull) {
     LW_CHECK_NULL(bar); // Does not throw.
     LW_CHECK_NULL(foo); // Does throw.
   } catch (const InvalidArgument& err) {
-    EXPECT_THAT(err.what(), MatchesRegex(".*foo must not be null.*"));
+    EXPECT_THAT(
+      err.what(), MatchesRegex(".*foo must not be null.*")
+    );
   } catch (...) {
     FAIL() << "Should throw InvalidArgument.";
+  }
+}
+
+TEST(ErrMacros, InternalNullCheckThrowsOnNull) {
+  try {
+    void* foo = nullptr;
+    void* bar = &foo;
+    LW_CHECK_NULL_INTERNAL(bar) << "Does not throw.";
+    LW_CHECK_NULL_INTERNAL(foo) << "Does throw.";
+  } catch (const Internal& err) {
+    EXPECT_THAT(err.what(), MatchesRegex(".*Does throw."));
+  } catch (...) {
+    FAIL() << "Should throw Internal.";
   }
 }
 
