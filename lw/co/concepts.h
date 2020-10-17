74395400b9a8862f8aede93d14cb80b83f9700d3
Natalie Wolfe <natalie@lifewanted.com>
2020-10-16 20:48:22 -0700

    Fixed await_suspend check for Awaitable concept.

--- lw/co/concepts.h
+++ lw/co/concepts.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <coroutine>
 #include <type_traits>
 
 namespace lw::co {
@@ -12,7 +13,7 @@ concept BooleanTestable = std::is_convertible_v<T, bool>;
 template <typename T>
 concept Awaitable = requires(T a) {
   { a.await_ready() } -> BooleanTestable;
-  { a.await_suspend() };
+  { a.await_suspend(std::declval<std::coroutine_handle<>>()) };
   { a.await_resume() };
 };
 
