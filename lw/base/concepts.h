2d961e27f6e6e2a1cd549e3aeaa7c4a89d08c65d
Natalie Wolfe <natalie@lifewanted.com>
2021-01-22 19:36:23 -0800

    Fixed iterables concepts

--- lw/base/concepts.h
+++ lw/base/concepts.h
@@ -5,16 +5,28 @@
 
 namespace lw {
 
+template <typename T, typename V>
+concept IterableAs =
+  requires(const T& a) {
+    { *std::begin(a) } -> std::same_as<const V&>;
+  } ||
+  requires(const T& a) {
+    { a.begin() } -> std::same_as<const V&>;
+  };
+
 template <typename T>
-concept ForwardIterable = requires(const T& a) {
-  { a.begin() } -> std::incrementable;
-  { a.end() };
-  { a.begin() != a.end() } -> std::convertible_to<bool>;
-};
+concept ForwardIterable =
+  requires(const T& a) {
+    { std::begin(a) } -> std::incrementable;
+    { std::end(a) };
+  } ||
+  requires(const T& a) {
+    { a.begin() } -> std::incrementable;
+    { a.end() };
+    { a.begin() != a.end() } -> std::convertible_to<bool>;
+  };
 
 template <typename T, typename V>
-concept ForwardIterableAs = ForwardIterable<T> && requires(const T& a) {
-  { *a.begin() } -> std::same_as<const V&>;
-};
+concept ForwardIterableAs = ForwardIterable<T> && IterableAs<T, V>;
 
 }
