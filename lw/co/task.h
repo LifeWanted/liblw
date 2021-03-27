a935409d6e73e35a75494fe4762b4f3670d47b15
Natalie Wolfe <natalie@lifewanted.com>
2021-03-27 09:57:00 -0700

    Add task callbacks and make Task always void.

--- lw/co/task.h
+++ lw/co/task.h
@@ -2,68 +2,64 @@
 
 #include <coroutine>
 #include <exception>
+#include <functional>
+#include <memory>
 
-#include "lw/co/coroutine_promise.h"
 #include "lw/err/canonical.h"
 
 namespace lw::co {
 
-template <typename T>
-class Task {
+class Task;
+
+class TaskPromise {
 public:
-  using promise_type = CoroutinePromise<Task<T>>;
-  using handle_type = std::coroutine_handle<promise_type>;
+  TaskPromise(): _state{std::make_shared<State>()} {};
+  ~TaskPromise() = default;
 
-  explicit Task(handle_type handle): _handle{std::move(handle)} {}
-  ~Task() {
-    if (_handle.promise()._error) {
-      std::rethrow_exception(_handle.promise()._error);
-    }
+  TaskPromise(TaskPromise&&) = delete;
+  TaskPromise(const TaskPromise&) = delete;
+  TaskPromise& operator=(TaskPromise&&) = delete;
+  TaskPromise& operator=(const TaskPromise&) = delete;
+
+  Task get_return_object();
+
+  auto initial_suspend() const { return std::suspend_always{}; }
+  auto final_suspend() const { return std::suspend_always{}; }
+
+  void unhandled_exception() { _state->error = std::current_exception(); }
+  void return_void() {
+    _state->finished = true;
+    if (_state->callback) _state->callback();
   }
 
-  bool done() const {
-    return _handle.done();
-  }
-
-  bool resume() {
-    _handle.resume();
-    return !_handle.done();
-  }
-
-  T get() {
-    if (!_handle.promise()._valid) {
-      throw FailedPrecondition()
-        << "Task is not in a valid state before fetching the value. Check "
-           "Task::done() before calling Task::get() and only call Task::get() "
-           "once per Task instance.";
-    }
-    _handle.promise()._valid = false;
-    if (_handle.promise()._error) {
-      std::exception_ptr error{_handle.promise()._error};
-      _handle.promise()._error = nullptr;
-      std::rethrow_exception(error);
-    }
-    return std::move(*_handle.promise()._value);
-  }
-
-  std::coroutine_handle<> handle() const {
-    return static_cast<std::coroutine_handle<>>(_handle);
-  }
+  bool valid() const { return _state->error || _state->finished; }
+  std::exception_ptr error() const { return _state->error; }
 
 private:
-  handle_type _handle;
+  friend class Task;
+  struct State {
+    std::exception_ptr error;
+    std::function<void()> callback;
+    bool finished = false;
+  };
+  std::shared_ptr<State> _state;
 };
 
-template <>
-class Task<void> {
+// -------------------------------------------------------------------------- //
+
+class Task {
 public:
-  using promise_type = CoroutinePromise<Task<void>>;
+  using promise_type = TaskPromise;
   using handle_type = std::coroutine_handle<promise_type>;
 
-  explicit Task(handle_type handle): _handle{std::move(handle)} {}
+  explicit Task(handle_type handle, std::shared_ptr<TaskPromise::State> state):
+    _handle{std::move(handle)},
+    _state{std::move(state)}
+  {}
+
   ~Task() {
-    if (_handle.promise()._error) {
-      std::rethrow_exception(_handle.promise()._error);
+    if (_handle.promise().error()) {
+      std::rethrow_exception(_handle.promise().error());
     }
   }
 
@@ -77,26 +73,34 @@ public:
   }
 
   void get() {
-    if (!_handle.promise()._valid) {
+    if (!_handle.promise().valid()) {
       throw FailedPrecondition()
         << "Task is not in a valid state before fetching the value. Check "
            "Task::done() before calling Task::get() and only call Task::get() "
            "once per Task instance.";
     }
-    _handle.promise()._valid = false;
-    if (_handle.promise()._error) {
-      std::exception_ptr error{_handle.promise()._error};
-      _handle.promise()._error = nullptr;
-      std::rethrow_exception(error);
+    if (_handle.promise().error()) {
+      std::rethrow_exception(_handle.promise().error());
     }
   }
 
+  promise_type& promise() { return _handle.promise(); }
   std::coroutine_handle<> handle() const {
     return static_cast<std::coroutine_handle<>>(_handle);
   }
 
+  template <typename Func>
+  void then(Func&& callback) {
+    _state->callback = std::forward<Func>(callback);
+  }
+
 private:
   handle_type _handle;
+  std::shared_ptr<TaskPromise::State> _state;
 };
+
+Task TaskPromise::get_return_object() {
+  return Task{Task::handle_type::from_promise(*this), _state};
+}
 
 }
