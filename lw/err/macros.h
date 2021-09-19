495378d591c8548cfafd582ab5a37096f4006242
Natalie Wolfe <natalie@lifewanted.com>
2021-09-19 11:02:56 -0700

    Added checks for > and >=.

--- lw/err/macros.h
+++ lw/err/macros.h
@@ -2,9 +2,36 @@
 
 #include "lw/err/canonical.h"
 
+#define _LW_UNWRAP_STRING_IMPL(x) #x
+#define _LW_UNWRAP_STRING(x) _LW_UNWRAP_STRING_IMPL x
+
 #define LW_CHECK_NULL(arg)  \
   while ((arg) == nullptr)  \
     throw lw::InvalidArgument() << #arg << " must not be null. "
 
 #define LW_CHECK_NULL_INTERNAL(arg) \
   while ((arg) == nullptr) throw lw::Internal()
+
+#define _LW_CHECK_GT_TYPED_IMPL(rhs, lhs, ErrorClass)                     \
+  while (!((rhs) > (lhs)))                                                \
+    throw ErrorClass()                                                    \
+      << "Expected `" _LW_UNWRAP_STRING(rhs) "` (" << rhs                 \
+      << ") > `" _LW_UNWRAP_STRING(lhs) "` (" << lhs << ") to be true. "
+
+#define LW_CHECK_GT_TYPED(rhs, lhs, ErrorClass) \
+  _LW_CHECK_GT_TYPED_IMPL((rhs), (lhs), ErrorClass)
+
+#define LW_CHECK_GT(rhs, lhs) \
+  _LW_CHECK_GT_TYPED_IMPL((rhs), (lhs), ::lw::FailedPrecondition)
+
+#define _LW_CHECK_GTE_TYPED_IMPL(rhs, lhs, ErrorClass)                    \
+  while (!((rhs) >= (lhs)))                                               \
+    throw ErrorClass()                                                    \
+      << "Expected `" _LW_UNWRAP_STRING(rhs) "` (" << rhs                 \
+      << ") >= `" _LW_UNWRAP_STRING(lhs) "` (" << lhs << ") to be true. "
+
+#define LW_CHECK_GTE_TYPED(rhs, lhs, ErrorClass) \
+  _LW_CHECK_GTE_TYPED_IMPL((rhs), (lhs), ErrorClass)
+
+#define LW_CHECK_GTE(rhs, lhs) \
+  _LW_CHECK_GTE_TYPED_IMPL((rhs), (lhs), ::lw::FailedPrecondition)
--- lw/err/macros_test.cpp
+++ lw/err/macros_test.cpp
@@ -9,7 +9,7 @@ namespace {
 
 using testing::MatchesRegex;
 
-TEST(ErrMacros, NullCheckThrowsOnNull) {
+TEST(ErrorMacros, NullCheckThrowsOnNull) {
   try {
     void* foo = nullptr;
     void* bar = &foo;
@@ -24,7 +24,7 @@ TEST(ErrMacros, NullCheckThrowsOnNull) {
   }
 }
 
-TEST(ErrMacros, InternalNullCheckThrowsOnNull) {
+TEST(ErrorMacros, InternalNullCheckThrowsOnNull) {
   try {
     void* foo = nullptr;
     void* bar = &foo;
@@ -32,6 +32,72 @@ TEST(ErrMacros, InternalNullCheckThrowsOnNull) {
     LW_CHECK_NULL_INTERNAL(foo) << "Does throw.";
   } catch (const Internal& err) {
     EXPECT_THAT(err.what(), MatchesRegex(".*Does throw."));
+  } catch (...) {
+    FAIL() << "Should throw Internal.";
+  }
+}
+
+TEST(ErrorMacros, CheckGreaterThan) {
+  try {
+    int small = 1;
+    int big = 42;
+    LW_CHECK_GT(big, small) << "Does not throw.";
+    LW_CHECK_GT(small, big) << "Does throw.";
+  } catch (const FailedPrecondition& err) {
+    EXPECT_THAT(
+      err.what(),
+      MatchesRegex(".*`small` \\(1\\) > `big` \\(42\\).*Does throw.")
+    );
+  } catch (...) {
+    FAIL() << "Should throw FailedPrecondition.";
+  }
+}
+
+TEST(ErrorMacros, CheckGreaterThanTyped) {
+  try {
+    int small = 1;
+    int big = 42;
+    LW_CHECK_GT_TYPED(big, small, Internal) << "Does not throw.";
+    LW_CHECK_GT_TYPED(small, big, Internal) << "Does throw.";
+  } catch (const Internal& err) {
+    EXPECT_THAT(
+      err.what(),
+      MatchesRegex(".*`small` \\(1\\) > `big` \\(42\\).*Does throw.")
+    );
+  } catch (...) {
+    FAIL() << "Should throw Internal.";
+  }
+}
+
+TEST(ErrorMacros, CheckGreaterThanOrEqualTo) {
+  try {
+    int small = 1;
+    int big = 42;
+    LW_CHECK_GTE(big, small) << "Does not throw.";
+    LW_CHECK_GTE(big, big) << "Does not throw.";
+    LW_CHECK_GTE(small, big) << "Does throw.";
+  } catch (const FailedPrecondition& err) {
+    EXPECT_THAT(
+      err.what(),
+      MatchesRegex(".*`small` \\(1\\) >= `big` \\(42\\).*Does throw.")
+    );
+  } catch (...) {
+    FAIL() << "Should throw FailedPrecondition.";
+  }
+}
+
+TEST(ErrorMacros, CheckGreaterThanOrEqualToTyped) {
+  try {
+    int small = 1;
+    int big = 42;
+    LW_CHECK_GTE_TYPED(big, small, Internal) << "Does not throw.";
+    LW_CHECK_GTE_TYPED(big, big, Internal) << "Does not throw.";
+    LW_CHECK_GTE_TYPED(small, big, Internal) << "Does throw.";
+  } catch (const Internal& err) {
+    EXPECT_THAT(
+      err.what(),
+      MatchesRegex(".*`small` \\(1\\) >= `big` \\(42\\).*Does throw.")
+    );
   } catch (...) {
     FAIL() << "Should throw Internal.";
   }
